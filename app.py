eb2d5f2a95f38530cfc4e9b279c0aa37ad6ae546
leadd <ddeal1@pm.me>
2025-06-24 19:40:18 -0500

    Adding customer wall paper, changing logo, modified footer link layout.

---
+++ app.py
@@ -0,0 +1,17 @@
+from dotenv import load_dotenv
+from flask import Flask, render_template
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("MY_SECRET_KEY")
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
