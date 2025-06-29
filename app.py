3de7aa7e6bdb4563a53ff9d31a8e905a07cf965a
leadd <ddeal1@pm.me>
2025-06-29 11:04:26 -0500

    Added route for blog page, additional formatting and styling, flaskified the named routes and file links.

--- app.py
+++ app.py
@@ -13,5 +13,40 @@ def home():
     return render_template("index.html")
 
 
+@app.route("/blog")
+def blog_posts():
+    dummy_posts = [
+        {
+            "id": 1,
+            "title": "Getting started with Flask",
+            "subtitle": "A quick walkthrough or routes, views and templates.",
+            "author": {"name": "David Deal"},
+            "date": "June 25, 2025",
+        },
+        {
+            "id": 2,
+            "title": "Test 2",
+            "subtitle": "Just testing layout.",
+            "author": {"name": "David Deal"},
+            "date": "June 26, 2025",
+        },
+        {
+            "id": 3,
+            "title": "Test 3",
+            "subtitle": "Before setting up the DB.",
+            "author": {"name": "David Deal"},
+            "date": "June 27, 2025",
+        },
+        {
+            "id": 4,
+            "title": "Test 4",
+            "subtitle": "And then maybe deploy.",
+            "author": {"name": "David Deal"},
+            "date": "June 28, 2025",
+        },
+    ]
+    return render_template("blog.html", all_posts=dummy_posts)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
