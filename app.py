e4452d380bc5ed14226fc8b92090967cdf03867d
leadd <ddeal1@pm.me>
2025-07-05 15:01:04 -0500

    Add register route.

--- app.py
+++ app.py
@@ -1,31 +1,110 @@
 from dotenv import load_dotenv
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-
+from flask import Flask, render_template, redirect, url_for, flash
 from sqlalchemy.orm import DeclarativeBase, relationship, Mapped, mapped_column
+from flask_login import LoginManager, login_user, UserMixin
 import os
+from forms import RegisterForm, LoginForm
+from flask_bootstrap import Bootstrap5
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Integer, String, Text, ForeignKey
+from typing import List
+from datetime import datetime
+from werkzeug.security import generate_password_hash
+from slugify import slugify
+
 
 load_dotenv()
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = os.getenv("MY_SECRET_KEY")
+app.config["SECRET_KEY"] = os.getenv("MY_SECRET_KEY", "dev_key_for_local_testing")
 
+bootstrap = Bootstrap5(app)
 
+#############################################
+# Configure Flask login manager
+#############################################
+# login_manager = LoginManager()
+# login_manager.init_app(app)
+
+#############################################
 # Create database
+#############################################
 class Base(DeclarativeBase):
     pass
-
 
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URI", "sqlite:///posts.db")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
+#############################################
+# Configure models
+#############################################
+class User(UserMixin, db.Model):
+    __tablename__ = "users"
 
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    email: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String(100), nullable=False)
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+    admin: Mapped[bool] = mapped_column(default=False, nullable=False)
+    is_active: Mapped[bool] = mapped_column(default=True, nullable=False)
+
+    posts: Mapped[List["BlogPost"]] = relationship(back_populates="author")
+    comments: Mapped[List["Comment"]] = relationship(back_populates="comment_author")
+
+
+class BlogPost(db.Model):
+    __tablename__ = "blog_posts"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    author_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+    author: Mapped["User"] = relationship(back_populates="posts")
+
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    subtitle: Mapped[str] = mapped_column(String(250), nullable=False)
+    date: Mapped[datetime] = mapped_column(default=datetime.utcnow, nullable=False)
+    updated_at: Mapped[datetime] = mapped_column(
+        default=datetime.utcnow, onupdate=datetime.utcnow
+    )
+    is_published: Mapped[bool] = mapped_column(default=True, nullable=False)
+    body: Mapped[str] = mapped_column(Text, nullable=False)
+    img_url: Mapped[str] = mapped_column(String(250), nullable=True)
+    slug: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+
+    comments: Mapped[List["Comment"]] = relationship(
+        back_populates="parent_post",
+        cascade="all, delete",  # Auto delete comments if a post is deleted
+    )
+
+    def generate_slug(self):
+        self.slug = slugify(self.title)
+
+
+class Comment(db.Model):
+    __tablename__ = "comments"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    comment: Mapped[str] = mapped_column(String(1000), nullable=False)
+
+    commenter_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+    comment_author: Mapped["User"] = relationship(back_populates="comments")
+    created_at: Mapped[datetime] = mapped_column(default=datetime.utcnow)
+
+    post_id: Mapped["BlogPost"] = mapped_column(
+        ForeignKey("blog_posts.id"), nullable=False
+    )
+    parent_post: Mapped["BlogPost"] = relationship(back_populates="comments")
+
+#############################################
+# Routes
+#############################################
+
+# Home page
 @app.route("/")
 def home():
     return render_template("index.html")
 
-
+# Blog main page
 @app.route("/blog")
 def blog_posts():
     dummy_posts = [
@@ -59,6 +138,31 @@ def blog_posts():
         },
     ]
     return render_template("blog.html", all_posts=dummy_posts)
+
+# Register new user
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    registration_form = RegisterForm()
+    if registration_form.validate_on_submit():
+        user_email = registration_form.email.data
+        if db.session.execute(db.Select(User).where(User.email == user_email)).scalar_one_or_none():
+            flash("An account with that email is already registered. Please login")
+            # return redirect(url_for("login"))
+        else:
+            hashed_and_salted_pw = generate_password_hash(
+                registration_form.password.data, method="pbkdf2:sha256", salt_length=10
+            )
+            new_user = User(
+                email=user_email,
+                password=hashed_and_salted_pw,
+                name=registration_form.name.data,
+            )
+            db.session.add(new_user)
+            db.session.commit()
+
+            login_user(new_user)
+            return redirect(url_for("blog_posts"))
+    return render_template("register.html", form=registration_form)
 
 
 if __name__ == "__main__":
---
+++ forms.py
@@ -0,0 +1,23 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length, URL
+
+
+class RegisterForm(FlaskForm):
+    name = StringField("Your Name", validators=[DataRequired()])
+    email = StringField("Your Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Enter a password", validators=[DataRequired()])
+    submit = SubmitField("Register")
+
+
+class LoginForm(FlaskForm):
+    email = StringField("Enter your email", validators=[DataRequired()])
+    password = PasswordField("Enter your password", validators=[DataRequired()])
+    login = SubmitField("Login")
+
+
+# class CreatePostForm(FlaskForm):
+#     title = StringField("Blog Post Title", validators=[DataRequired()])
+#     subtitle = StringField("Subtitle", validators=[DataRequired()])
+#     img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
+#     body =
